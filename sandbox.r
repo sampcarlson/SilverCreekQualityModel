efe0cf7de5423ad912b820d8b81796fb2737a1b8
Sam Carlson <sam.p.carlson@gmail.com>
2023-01-25 16:43:44 -0700

    Create sandbox.r

---
+++ sandbox.r
@@ -0,0 +1,17 @@
+source("~/Documents/R Workspace/SilverCreekQualityModel/functions.r")
+
+dbGetQuery(conn(),"SELECT * FROM data LIMIT 10;")
+
+flowid=dbGetQuery(conn(),"SELECT metricid FROM metrics WHERE name = 'flow';")$metricid
+
+flowLocations=dbGetQuery(conn(),paste0("SELECT DISTINCT locationid from DATA where metricid = '",flowid,"';"))
+
+flowLocations=dbGetQuery(conn(),paste0("SELECT * FROM locations WHERE locationid IN ('",
+                                       paste0(flowLocations$locationid,collapse="', '"),
+                                       "');"))
+
+
+
+dataSeries=listDataSeries()
+
+seriesData=getSeriesData(dataSeries$seriesid[dataSeries$metric=="flow"])
