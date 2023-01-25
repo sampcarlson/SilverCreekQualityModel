96011a4f242ab4312491dce28b48440628b4a640
Sam Carlson <sam.p.carlson@gmail.com>
2023-01-25 16:43:04 -0700

    Update functions.r

--- functions.r
+++ functions.r
@@ -28,3 +28,37 @@ conn=function(){    #checks for db connection object ('scdb') in global env, cre
 }
 
 
+listDataSeries=function(){
+  #allSeries=dbGetQuery(conn(),"SELECT DISTINCT locationid, metricid FROM data;")
+  allSeries=dbGetQuery(conn(),"SELECT DISTINCT metricid, locationid, MIN(datetime) ,MAX(datetime), COUNT(datetime) FROM data GROUP BY metricid, locationid;")
+  allSeries=merge(allSeries, dbGetQuery(conn(),"SELECT metricid, name FROM metrics;"))
+  allSeries$seriesid=1:nrow(allSeries)
+  allSeries=allSeries[,c("seriesid","locationid","metricid","name","count","min","max")]
+  names(allSeries)[4]="metric"
+  names(allSeries)[6]="startdate"
+  names(allSeries)[7]="enddate"
+  return(allSeries)
+}  
+
+getSeriesData=function(seriesids,dataSeriesList=dataSeries){
+  thisSeries=dataSeriesList[dataSeriesList$seriesid %in% seriesids,]
+  seriesData=dbGetQuery(conn(),paste0("SELECT * FROM data WHERE 
+                             locationid IN ('",paste0(thisSeries$locationid,collapse="', '"),"' ) 
+                             AND metricid  IN ('",paste0(thisSeries$metricid,collapse="', '"),"' ) 
+                             ORDER BY datetime;")
+  )
+  seriesData=merge(thisSeries,seriesData,by=c("locationid","metricid","metric"))
+  return(seriesData)
+}
+
+
+plotDataSeries=function(seriesData){  #currently limited to 1 metric
+  if(length(unique(seriesData$metricid))>1){
+    print(paste0("Plotting only ",seriesData$metric[seriesData$metricid==unique(seriesData$metricid)[1]][1],", ignoring all other metrics"))
+  }
+  seriesData=seriesData[seriesData$metricid == unique(seriesData$metricid)[1],]
+  plot(seriesData$value~seriesData$datetime,type="n")
+  colorScheme=data.frame(seriesid=unique(seriesData$seriesid),color=sample(colors(),length(unique(seriesData$seriesid))))
+  for(i in 1:length(unique(seriesData$seriesid))){}
+  
+}
